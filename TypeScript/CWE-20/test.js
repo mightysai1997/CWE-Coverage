d1b234413151d6d2c2ca2bd6cc70b4e1f09418de
mightysai1997 <110396758+mightysai1997@users.noreply.github.com>
2024-01-08 15:32:03 +0530

    Update test.js

--- TypeScript/CWE-20/test.js
+++ TypeScript/CWE-20/test.js
@@ -1,9 +1,8 @@
-app.get('/some/path', function(req, res) {
-    let url = req.param('url'),
-        host = urlLib.parse(url).host;
-    // BAD: the host of `url` may be controlled by an attacker
-    let regex = /^((www|beta).)?example.com/;
-    if (host.match(regex)) {
-        res.redirect(url);
+function filterScript(html) {
+    var scriptRegex = /<script\b[^>]*>([\s\S]*?)<\/script>/gi;
+    var match;
+    while ((match = scriptRegex.exec(html)) !== null) {
+        html = html.replace(match[0], match[1]);
     }
-});
+    return html;
+}
