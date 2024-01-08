1e4082e51fd7a563dd9954472ae9927e16a04894
mightysai1997 <110396758+mightysai1997@users.noreply.github.com>
2024-01-08 15:19:46 +0530

    Update IncompleteHostnameRegExp.ts

--- TypeScript/CWE-20/IncompleteHostnameRegExp.ts
+++ TypeScript/CWE-20/IncompleteHostnameRegExp.ts
@@ -1,26 +1,16 @@
-import express, { Request, Response } from 'express';
+import * as express from 'express';
 import * as urlLib from 'url';
 
 const app = express();
 
-app.get('/some/path', function(req: Request, res: Response) {
-    let url: string | undefined = req.query.url as string;
-    let host: string | null = null;
-
-    if (url) {
-        host = urlLib.parse(url).host;
-    }
+app.get('/some/path', function (req, res) {
+    const url: string = req.param('url');
+    const host: string | null = urlLib.parse(url).host;
 
     // BAD: the host of `url` may be controlled by an attacker
-    let regex = /^((www|beta).)?example.com/;
+    const regex: RegExp = /^((www|beta).)?example.com/;
 
     if (host && host.match(regex)) {
         res.redirect(url);
     }
-});
-
-// Example of using the express application
-const PORT = 3000;
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
 });
