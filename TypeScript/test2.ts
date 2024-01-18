a0576e7de5db44d2d9c7f712ed4127a8a1898a4f
mightysai1997 <110396758+mightysai1997@users.noreply.github.com>
2024-01-18 15:05:26 +0530

    Update test2.ts

--- TypeScript/test2.ts
+++ TypeScript/test2.ts
@@ -1,10 +1,18 @@
-function bufferOverReadExample() {
-  const buffer = Buffer.from('Hello, World!');
-
-  // Attempt to read more bytes than the buffer contains
-  const result = buffer.toString('utf-8', 0, 50);
-
-  console.log(result);
-}
-
-bufferOverReadExample();
+const buffer = new Buffer(5);
+ 
+buffer[0] = 255;
+console.log(buffer[0]); // 255
+ 
+buffer[1] = 256;
+console.log(buffer[1]); // 0
+ 
+buffer[2] = 260;
+console.log(buffer[2]); // 4
+console.log(buffer[2] === 260%256); // true
+ 
+buffer[3] = 516;
+console.log(buffer[3]); // 4
+console.log(buffer[3] === 516%256); // true
+ 
+buffer[4] = -50;
+console.log(buffer[4]); // 206
