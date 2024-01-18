09754a86ebafefe8a959b4ec47795cf325d63776
mightysai1997 <110396758+mightysai1997@users.noreply.github.com>
2024-01-18 14:53:29 +0530

    Update test.ts

--- TypeScript/test.ts
+++ TypeScript/test.ts
@@ -1,13 +1,17 @@
 angular.module("app")
-    .controller("Controller", function($cacheFactory, $templateCache, $cookies) {
-        // Insecure: Using plain text passwords without encryption
+    .controller("controller", function($cacheFactory, $templateCache, $cookies) {
+        // NOT OK - Storing passwords directly in cache or template cache is not recommended
         $cacheFactory.put("x", data1.password);
         $templateCache.put("x", data2.password);
+
+        // NOT OK - Storing passwords directly in cookies is not secure
         $cookies.put("x", data3.password);
         $cookies.putObject("x", data4.password);
 
-        // OK: Demonstrating other AngularJS functions
+        // OK - Storing passwords in other variables is acceptable
         $cookies.other("x", data5.password);
         other.put("x", data6.password);
+        
+        // OK - Storing passwords as keys (not values) in cookies is acceptable
         $cookies.put(data7.password, "x");
     });
