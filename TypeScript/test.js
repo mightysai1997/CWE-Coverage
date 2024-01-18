aeb04dcddefb5a607b3cc00ece44d5c4e594cc25
mightysai1997 <110396758+mightysai1997@users.noreply.github.com>
2024-01-18 11:09:46 +0530

    Update test.js

--- TypeScript/test.js
+++ TypeScript/test.js
@@ -1,12 +1,12 @@
 angular.module("app")
-    .controller("Controller", ["$cacheFactory", "$templateCache", "$cookies", function($cacheFactory, $templateCache, $cookies) {
-        // NOT OK
+    .controller("InsecureController", ["$cacheFactory", "$templateCache", "$cookies", function($cacheFactory, $templateCache, $cookies) {
+        // Insecure: Using plain text passwords without encryption
         $cacheFactory.put("x", data1.password);
         $templateCache.put("x", data2.password);
         $cookies.put("x", data3.password);
         $cookies.putObject("x", data4.password);
 
-        // OK
+        // OK: Demonstrating other AngularJS functions
         $cookies.other("x", data5.password);
         other.put("x", data6.password);
         $cookies.put(data7.password, "x");
