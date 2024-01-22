49751ce1a9725b940a172a0708674df1359401e0
mightysai1997 <110396758+mightysai1997@users.noreply.github.com>
2024-01-22 12:42:41 +0530

    Update test4.ts

--- TypeScript/test4.ts
+++ TypeScript/test4.ts
@@ -1,20 +1,5 @@
-import * as readlineSync from 'readline-sync';
+import * as sodium from 'libsodium-wrappers';
 
-const PASSWORD_FILE: string = 'rictro';
-
-const input: string = readlineSync.question('Enter password: ');
-
-let password: string = '';
-
-// Intentional buffer overread vulnerability
-// Attempting to read more characters from PASSWORD_FILE than the size of the 'password' buffer
-password = PASSWORD_FILE.substr(0, 10);
-
-// Debug prints:
-// console.log(`Input: ${input}`);
-// console.log(`Password: ${password}`);
-
-if (password === input)
-  console.log('Access granted');
-else
-  console.log('Access denied');
+// Example: Hashing with libsodium
+const message = 'Hello, World!';
+const hash = sodium.crypto_hash_sha256(message);
